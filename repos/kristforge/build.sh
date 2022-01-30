6a699a90408939dd0d173046a699f0e07ff19430
RubenHetKonijn <rubenknijn@protonmail.com>
2022-01-30 09:46:45 +0100

    fix the kristforge repo build script

--- repos/kristforge/build.sh
+++ repos/kristforge/build.sh
@@ -4,7 +4,10 @@ cd kristforge
 cross build --target x86_64-pc-windows-gnu --release
 cross build --target x86_64-unknown-linux-gnu --release
 
-# copy build to the folder
+# copy build to the folders
 
-mkdir ../../build/kristforge/linux_x86_64 -p
+mkdir ../../../builds/kristforge/linux_x86_64 -p
 cp target/x86_64-unknown-linux-gnu/release/kristforge ../../build/kristforge/linux_x86_64
+
+mkdir ../../../builds/kristforge/windows_x86_64 -p
+cp target/x86_64-pc-windows-gnu/release/kristforge.exe ../../builds/kristforge/windows_x86_64
